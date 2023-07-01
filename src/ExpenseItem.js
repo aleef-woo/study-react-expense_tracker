483ecd8ae179f34836311abebca7687e8c155835
aleef-woo <aleeffirdaus30@gmail.com>
2023-07-01 17:03:03 +0800

    Update: add expeseItem component

---
+++ src/ExpenseItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './ExpenseItem.css'
+
+const ExpenseItem = () => {
+    const expenseDate = new Date(2023, 7, 1);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 250.50;
+
+  return (
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">RM{expenseAmount}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseItem;
