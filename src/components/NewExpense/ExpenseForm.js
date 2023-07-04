ab9deaed9d926c31671cde5602a44d688fb3077c
aleef-woo <aleeffirdaus30@gmail.com>
2023-07-04 16:33:43 +0800

    Update: add form handler to handle adding new expense

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -99,6 +99,7 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button onClick={props.onCancelAddNewExpense}>Cancel</button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [addNewExpense, setAddNewExpense] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setAddNewExpense(false);
+  };
+
+  const formHandler = () => {
+    setAddNewExpense((prevState) => !prevState);
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!addNewExpense && <button onClick={formHandler}>Add New Expense</button>}
+      {addNewExpense && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancelAddNewExpense={formHandler}
+        />
+      )}
     </div>
   );
 };
